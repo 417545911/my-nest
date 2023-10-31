121333c750677cadb69d415a8d0d6729a7a013be
ljy <4175459@qq.com>
2023-10-31 16:13:42 +0800

    feat: 增加swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions = {cors:true};
+  const app = await NestFactory.create(AppModule,appOptions);
+  app.setGlobalPrefix("api");
+
+  const options = new DocumentBuilder()
+      .setTitle("Nest API 模拟")
+      .setDescription("前端模拟接口")
+      .setVersion("1.0")
+      .setBasePath("api")
+      .addBearerAuth()
+      .build();
+  const document = SwaggerModule.createDocument(app,options);
+  SwaggerModule.setup("/docs",app,document)
+  
   await app.listen(3000);
 }
 bootstrap();
+
