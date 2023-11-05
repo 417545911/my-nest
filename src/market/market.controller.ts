40d438ffcd792493b84d58c2df74dbf30e4f2906
liujiyou <>
2023-11-05 22:54:45 +0800

    feat: 增加涨跌接口get

--- src/market/market.controller.ts
+++ src/market/market.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller,Get} from '@nestjs/common';
 import { MarketService } from './market.service';
-import { CreateMarketDto } from './dto/create-market.dto';
-import { UpdateMarketDto } from './dto/update-market.dto';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('market') // 设置swagger文档用的,标记路由所属模块,用于区分api.
@@ -9,33 +7,14 @@ import { ApiTags } from '@nestjs/swagger';
 export class MarketController {
   constructor(private readonly marketService: MarketService) {}
 
-  @Post() // 声明接口的请求类型
-  create(@Body() createMarketDto: CreateMarketDto) {
-    return this.marketService.create(createMarketDto);
-  }
-
   @Get()
-  findAll() {
-    return this.marketService.findAll();
+  getMock(): string {
+    return this.marketService.getMock();
   }
 
-  @Get('getApiOne')
-  getApiOne(){
-    return this.marketService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.marketService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMarketDto: UpdateMarketDto) {
-    return this.marketService.update(+id, updateMarketDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.marketService.remove(+id);
+  @Get("getMock1")
+  getMock1() {
+    return this.marketService.getMock1();
   }
 }
+
--- src/market/market.service.ts
+++ src/market/market.service.ts
@@ -1,30 +1,53 @@
 import { Injectable } from '@nestjs/common';
-import { CreateMarketDto } from './dto/create-market.dto';
-import { UpdateMarketDto } from './dto/update-market.dto';
+// import dayjs from 'dayjs';
+const dayjs = require('dayjs');
 
 @Injectable()
 export class MarketService {
-  create(createMarketDto: CreateMarketDto) {
-    // return 'This action adds a new market';
-    return {
-      msg:"创建成功",
-      body:createMarketDto
+  getMock(): string {
+    return '235';
+  }
+  getMock1() {
+    const _date = Array.from({ length: 5 }, (_, i) =>
+      dayjs().subtract(4-i, 'day').format('YYYY-MM-DD'),
+    );
+    let secondValue = 0; // 涨跌初始值
+    const ret = _date.map((el) => generateTimeArray(el,secondValue));
+    return ret.flat();
+  }
+}
+
+function generateTimeArray(date, secondValue,startTime = '09:00', endTime = '17:30') {
+  date = dayjs(date).format('YYYY-MM-DD');
+  const timeArray = [];
+  const startHour = parseInt(startTime.split(':')[0]);
+  const startMinute = parseInt(startTime.split(':')[1]);
+  const endHour = parseInt(endTime.split(':')[0]);
+  const endMinute = parseInt(endTime.split(':')[1]);
+  for (let hour = startHour; hour <= endHour; hour++) {
+    let minuteStart = 0;
+    let minuteEnd = 59;
+
+    if (hour === startHour) {
+      minuteStart = startMinute;
+    }
+    if (hour === endHour) {
+      minuteEnd = endMinute;
+    }
+    for (let minute = minuteStart; minute <= minuteEnd; minute++) {
+      const formattedHour = hour.toString().padStart(2, '0');
+      const formattedMinute = minute.toString().padStart(2, '0');
+      const randomVal = Math.random() * 0.2 - 0.1
+      const _secondValue = secondValue - randomVal;
+      secondValue = Math.abs(_secondValue) > 4.4 ? secondValue : _secondValue;
+
+      timeArray.push([
+        `${date} ${formattedHour}:${formattedMinute}`,
+        +secondValue.toFixed(2),
+        null,
+      ]);
     }
   }
 
-  findAll() {
-    return `This action returns all market`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} market`;
-  }
-
-  update(id: number, updateMarketDto: UpdateMarketDto) {
-    return `This action updates a #${id} market`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} market`;
-  }
+  return timeArray;
 }
